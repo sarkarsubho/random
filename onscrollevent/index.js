ee8da327720f49b78eabedf851ddc70a48b33242
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-11-01 00:40:00 +0530

    scroll animation event

---
+++ onscrollevent/index.js
@@ -0,0 +1,30 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+     var revealsp = document.querySelectorAll(".revealp");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+    
+     for (var j = 0; j < revealsp.length; j++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = revealsp[j].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        revealsp[j].classList.add("active");
+      } else {
+        revealsp[j].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  

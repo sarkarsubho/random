9c38d5e11b8883931845ddbd51a021dc87e24f0e
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2023-01-26 00:21:23 +0530

    updated

---
+++ concept Explaner/Class/class.js
@@ -0,0 +1,66 @@
+// class Car {
+//     constructor(name, year) {
+//       this.name = name;
+//       this.year = year;
+//     }
+//     set current(name){
+//         this.name=name
+//     }
+//     get current(){
+//      return this.year
+//     }
+//   }
+//   let car=new Car("s","v");
+//   car.current="ok"
+
+//   console.log(car.current);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   Classes are a template for creating objects. They encapsulate data with code to work on that data. Classes in JS are built on prototypes but also have some syntax and semantics that are unique to classes.
+// The get syntax binds an object property to a function that will be called when that property is looked up. It can also be used in classes.
+// The set syntax binds an object property to a function to be called when there is an attempt to set that property. It can also be used in classes.
